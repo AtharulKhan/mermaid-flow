de9f7171340a65f5cdf145cffbfd624311ff27bb
Atharul Khan <atharul.k@gmail.com>
2026-02-15 14:15:22 -0500

    Increase gantt top margin further

--- src/App.jsx
+++ src/App.jsx
@@ -236,7 +236,7 @@ function getIframeSrcDoc() {
         display: grid;
         flex: 0 0 auto;
         position: relative;
-        margin-top: 10px;
+        margin-top: 16px;
         border-radius: 12px;
         border: 1px solid #d9dee8;
       }
